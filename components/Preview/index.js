a1dc8486ea4560fb770af72774e0c4626f3522b4
Ivan <ggwpdiaf@gmail.com>
2021-03-18 01:18:06 -0300

    fixed small bug

--- components/Preview/index.js
+++ components/Preview/index.js
@@ -3,7 +3,7 @@ import PostPreview from "./PostPreview";
 
 const Preview = ({ posts }) => {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col w-full">
       {posts && posts.map((post) => <PostPreview key={post.slug} {...post} />)}
     </div>
   );
