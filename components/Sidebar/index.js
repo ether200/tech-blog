286c0ac1cc2c4f06ee9fc188e2e44137b36a24c9
Ivan <ggwpdiaf@gmail.com>
2021-06-15 18:42:00 -0300

    fixed small typo and added sticky position to sidebar

--- components/Sidebar/index.js
+++ components/Sidebar/index.js
@@ -5,7 +5,10 @@ import Recent from "./Recent";
 
 const Sidebar = ({ recentPosts, categories }) => {
   return (
-    <aside className="grid grid-cols-auto gap-8 justify-center my-6 ll:w-60 ll:flex ll:flex-col ll:justify-start ll:space-y-4 ll:my-0">
+    <aside
+      className="grid grid-cols-auto gap-8 justify-center my-6 ll:w-60 ll:flex ll:flex-col ll:justify-start ll:space-y-4 ll:my-0 ll:sticky top-0"
+      style={{ height: "fit-content" }}
+    >
       <About />
       <Recent recentPosts={recentPosts} />
       <Categories categories={categories} />
